90d99a97c889bee1d1c4eaac1b69766ef943ffba
Børge Rokseth <Borge.Rokseth@sintef.no>
2021-02-12 05:55:46 +0100

    created exercise-model

--- django/exercise/models.py
+++ django/exercise/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Exercise(models.Model):
+    name = models.TextField("Name of exercise")
+    description = models.TextField("Description of exercise")
+    instruction_video = models.URLField(max_length=500)
+    silent = models.BooleanField(default=False)
+    equipment = models.BooleanField(default=False)
