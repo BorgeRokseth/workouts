39ace1975b496f4527b458fd1278160e53bddbf6
Børge Rokseth <Borge.Rokseth@sintef.no>
2021-02-16 06:47:48 +0100

    Made nav drawer adn app-bar work

--- workout-app/src/App.vue
+++ workout-app/src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar>
-      <v-app-bar-nav-icon @click="drawerControl" />
-      <v-toolbar-title>Workout Flows</v-toolbar-title>
-    </v-app-bar>
     <v-navigation-drawer v-model="drawer" app>
-    <v-list>
-      <v-subheader>MENU</v-subheader>
-      <v-list-item-group v-model="selectedItem">
-        <v-list-item v-for="(item, i) in menuItems" :key="i">
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-  </v-navigation-drawer>
+      <v-list>
+        <v-subheader>MENU</v-subheader>
+        <v-list-item-group v-model="selectedItem">
+          <v-list-item v-for="(item, i) in menuItems" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app dark>
+      <v-app-bar-nav-icon @click="drawerControl" />
+      <v-toolbar-title>Workout Flows </v-toolbar-title>
+    </v-app-bar>
   </v-app>
 </template>
 
@@ -27,23 +27,23 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component({
   components: {}
-  })
+})
 export default class App extends Vue {
   drawer = false;
   selectedItem = 1;
   menuItems = [
-        { text: `Exercise`, icon: `mdi-border-color` },
-        { text: `Flow`, icon: `mdi-airplane` },
-        { text: `Workout`, icon: `mdi-clock` }
-      ];
+    { text: `Exercise`, icon: `mdi-alpha-e-circle` },
+    { text: `Flow`, icon: `mdi-alpha-f-circle` },
+    { text: `Workout`, icon: `mdi-alpha-w-circle` }
+  ];
 
-  drawerControl(){
-    if (this.drawer === false){
+  drawerControl() {
+    if (this.drawer === false) {
       this.drawer = true;
-    } else{
+    } else {
       this.drawer = false;
     }
-    console.log(this.drawer)
+    console.log(this.drawer);
   }
 }
 </script>
---
+++ workout-app/src/views/ExerciseListView.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "ExerciseListView"
+}
+</script>
+
+<style scoped>
+
+</style>
