ec026f6055f846cb411442a132d9194d24102c54
Børge Rokseth <Borge.Rokseth@sintef.no>
2021-03-06 10:23:34 +0100

    Corrected allauth-socialaccount to allauth.socialaccount

--- django/workouts/settings.py
+++ django/workouts/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'allauth',
     'allauth.account',
-    'allauth-socialaccount',
+    'allauth.socialaccount',
 
     'rest_auth',
     'rest_auth.registration',
